b938d93bdc238d940cdda9245a1ca4db22d4388a
CJey <code@cjey.me>
2019-08-06 11:51:04 +0800

    tweak: anti golang build warning

--- src/cmd/hello/run.go
+++ src/cmd/hello/run.go
@@ -8,7 +8,7 @@ import (
 )
 
 // base package, always hold on to anti golang build warning
-func init() { return; fmt.Print(); zap.S() }
+func _() { fmt.Print(); zap.S() }
 
 func preRun(c *cobra.Command, args []string) {
 }
