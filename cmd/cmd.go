b3001132723e31c2e830a9ed62db5500a8151e67
CJey <cjey.hou@gmail.com>
2020-04-14 18:12:40 +0800

    feat: update gbase to version v0.0.2

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,15 +2,12 @@ package cmd
 
 import (
 	"errors"
-	"math/rand"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 
 	"github.com/cjey/gbase"
 
@@ -29,23 +26,14 @@ func init() {
 	_CMDRoot.Short = rpl.Replace(_CMDRoot.Short)
 	_CMDRoot.Long = rpl.Replace(_CMDRoot.Long)
 	_CMDRoot.Version = app.Version
-	supportConfigAndLogger(_CMDRoot)
 
-	cobra.OnInitialize(commandInitializer)
+	supportConfigAndLogger(_CMDRoot)
 }
 
 func Execute() {
 	if err := _CMDRoot.Execute(); err != nil {
 		os.Exit(1)
 	}
-}
-
-func commandInitializer() {
-	// initialize random generator
-	rand.Seed(gbase.BootTime.UnixNano())
-
-	// initialize default logger
-	setLogger("debug", "stderr", "console", false)
 }
 
 func supportConfigAndLogger(cmd *cobra.Command) {
@@ -109,76 +97,10 @@ func handleLogger(cmd *cobra.Command) {
 	viper.BindPFlag("log.encoding", cmd.Flags().Lookup("log-encoding"))
 	viper.BindPFlag("log.show-caller", cmd.Flags().Lookup("log-show-caller"))
 
-	setLogger(
+	gbase.ReplaceZapLogger(
 		viper.GetString("log.level"),
 		viper.GetString("log.file"),
 		viper.GetString("log.encoding"),
 		viper.GetBool("log.show-caller"),
 	)
-}
-
-func setLogger(lvl, outpath, encoding string, showCaller bool) {
-	var level zapcore.Level
-	switch lvl {
-	case "debug":
-		level = zap.DebugLevel
-	case "info":
-		level = zap.InfoLevel
-	case "warn":
-		level = zap.WarnLevel
-	case "error":
-		level = zap.ErrorLevel
-	case "panic":
-		level = zap.PanicLevel
-	case "fatal":
-		level = zap.FatalLevel
-	default:
-		zap.S().Fatalw("unexpected log level", "level", lvl)
-	}
-	l, err := logger(level, outpath, encoding, showCaller)
-	if err != nil {
-		zap.S().Fatalw("build logger fail", "err", err, "file", outpath)
-	}
-	zap.ReplaceGlobals(l)
-}
-
-func logger(level zapcore.Level, outpath, encoding string, showCaller bool) (*zap.Logger, error) {
-	zapCfg := zap.Config{
-		Level:            zap.NewAtomicLevelAt(level),
-		OutputPaths:      []string{outpath},
-		ErrorOutputPaths: []string{outpath},
-
-		Encoding: encoding,
-		EncoderConfig: zapcore.EncoderConfig{
-			TimeKey:    "T",
-			LevelKey:   "L",
-			NameKey:    "N",
-			MessageKey: "M",
-			LineEnding: "\n",
-
-			CallerKey:     "Caller",
-			StacktraceKey: "Stack",
-
-			EncodeLevel:    zapcore.LowercaseLevelEncoder,
-			EncodeDuration: zapcore.NanosDurationEncoder,
-			EncodeCaller:   zapcore.ShortCallerEncoder,
-			EncodeName:     zapcore.FullNameEncoder,
-			EncodeTime: func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
-				enc.AppendString(t.Format("2006-01-02 15:04:05.000"))
-			},
-		},
-
-		//Development:       true,
-		DisableCaller: !showCaller,
-		//DisableStacktrace: true,
-		//Sampling: &SamplingConfig{
-		//  Initial:    100,
-		//  Thereafter: 100,
-		//},
-		//InitialFields: map[string]interface{}{
-		//  "foo": "bar",
-		//},
-	}
-
-	return zapCfg.Build()
 }
